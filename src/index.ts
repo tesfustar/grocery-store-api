a6ce9e93229e7f3b9dc8e085889a1f72f3d727e7
Tesfaye Lemi <tesfu9503@gmail.com>
2023-02-28 23:59:01 -0800

    ready for test deployment

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,27 @@
-console.log("hh")
+import express, { Application, Request, Response, NextFunction } from 'express';
+import mongoose from 'mongoose';
+
+const app: Application = express();
+
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost:27017/mydatabase')
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((err) => console.error(err));
+
+// Middleware
+app.use(express.json());
+
+// Routes
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello, world!');
+});
+
+// Error handling middleware
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  console.error(err.stack);
+  res.status(500).send('Something went wrong!');
+});
+
+// Start the server
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Server listening on port ${port}`));
