85728720de3d98c450ff8bb4dd6e44b5df6d9fe8
Tesfaye Lemi <tesfu9503@gmail.com>
2023-03-01 00:01:52 -0800

    ready for test deployment error fix 01

--- src/index.ts
+++ src/index.ts
@@ -3,24 +3,13 @@ import mongoose from 'mongoose';
 
 const app: Application = express();
 
-// Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/mydatabase')
-  .then(() => console.log('Connected to MongoDB'))
-  .catch((err) => console.error(err));
 
-// Middleware
-app.use(express.json());
 
 // Routes
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello, world!');
 });
 
-// Error handling middleware
-app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-  console.error(err.stack);
-  res.status(500).send('Something went wrong!');
-});
 
 // Start the server
 const port = process.env.PORT || 3000;
