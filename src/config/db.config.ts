37d573ce4da331cab371a020af9eb85b2554cff0
Tesfaye Lemi <tesfu9503@gmail.com>
2023-03-15 02:52:58 -0700

    crud operation on category and product and also authentication with forgot password changed

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -1,11 +1,11 @@
 import mongoose from "mongoose";
+import dotenv from 'dotenv';
 
-mongoose.set("strictQuery", true);
+dotenv.config();
+mongoose.set("strictQuery", true)
 // 'mongodb://0.0.0.0:27017/grocery'
-// mongodb+srv://abdi:abdi9503@cluster0.idduthi.mongodb.net/?retryWrites=true&w=majority
-const mongoUrl: string = process.env.MONGO_URL as string;
 const mongoDbConnection = mongoose
-  .connect("mongodb+srv://abdi:abdi9503@cluster0.idduthi.mongodb.net/?retryWrites=true&w=majority")
+  .connect(process.env.MONGO_URL!)
   .then(() => console.log("db connected"))
   .catch((err) => console.log(`error ${err}`));
 
